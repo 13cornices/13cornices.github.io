8c3d10fc940bd96559cc33e24af045e6915f0fee
13cornices <13cornices@users.noreply.github.com>
2026-08-31 22:24:22 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 13cornices.github.io
-Subtracker app for soccer subbing
+# Soccer Sub Tracker PWA
+
+Upload every file in this folder to the same HTTPS folder on your website.
+
+Open `index.html` on the phone once while online. The service worker caches the app files, icons, and current CDN scripts. After that first successful load, the app should open from cache and continue working offline.
+
+On Android, open the site in Chrome and choose **Install app** or **Add to Home screen**.
+
+On iPhone, open the site in Safari, tap **Share**, then **Add to Home Screen**.
+
+To publish an update, replace the files on the website and change `CACHE_NAME` in `service-worker.js`, for example from `soccer-sub-tracker-v4` to `soccer-sub-tracker-v5`. Phones will pick up the new cache after they open the app online.
---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "soccer-sub-tracker-v4";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "https://unpkg.com/react@18.3.1/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18.3.1/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone@7.24.7/babel.min.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true })
+      .then(cached => {
+        if (cached) return cached;
+        return fetch(event.request).then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        });
+      })
+  );
+});
